9ed35874c4a51f06aa0bf62f9e30221027244c00
Miguel Coreas <coreas87259@hotmail.com>
2022-01-05 22:25:10 -0600

    base del proyecto

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,158 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HeaderComponent } from './header/header.component';
+/** NG_ZORRO */
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzCollapseModule } from 'ng-zorro-antd/collapse';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
+import { NzAffixModule } from 'ng-zorro-antd/affix';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzPaginationModule } from 'ng-zorro-antd/pagination';
+import { NzStepsModule } from 'ng-zorro-antd/steps';
+import { NzAutocompleteModule } from 'ng-zorro-antd/auto-complete';
+import { NzCascaderModule } from 'ng-zorro-antd/cascader';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
+import { NzMentionModule } from 'ng-zorro-antd/mention';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
+import { NzRateModule } from 'ng-zorro-antd/rate';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzSliderModule } from 'ng-zorro-antd/slider';
+import { NzSwitchModule } from 'ng-zorro-antd/switch';
+import { NzTimePickerModule } from 'ng-zorro-antd/time-picker';
+import { NzTransferModule } from 'ng-zorro-antd/transfer';
+import { NzTreeSelectModule } from 'ng-zorro-antd/tree-select';
+import { NzUploadModule } from 'ng-zorro-antd/upload';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzBadgeModule } from 'ng-zorro-antd/badge';
+import { NzCalendarModule } from 'ng-zorro-antd/calendar';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzCarouselModule } from 'ng-zorro-antd/carousel';
+import { NzCommentModule } from 'ng-zorro-antd/comment';
+import { NzDescriptionsModule } from 'ng-zorro-antd/descriptions';
+import { NzEmptyModule } from 'ng-zorro-antd/empty';
+import { NzImageModule } from 'ng-zorro-antd/image';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzPopoverModule } from 'ng-zorro-antd/popover';
+import { NzStatisticModule } from 'ng-zorro-antd/statistic';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NzTimelineModule } from 'ng-zorro-antd/timeline';
+import { NzTreeModule } from 'ng-zorro-antd/tree';
+import { NzTreeViewModule } from 'ng-zorro-antd/tree-view';
+import { NzAlertModule } from 'ng-zorro-antd/alert';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
+import { NzMessageModule } from 'ng-zorro-antd/message';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzNotificationModule } from 'ng-zorro-antd/notification';
+import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
+import { NzProgressModule } from 'ng-zorro-antd/progress';
+import { NzResultModule } from 'ng-zorro-antd/result';
+import { NzSkeletonModule } from 'ng-zorro-antd/skeleton';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzAnchorModule } from 'ng-zorro-antd/anchor';
+import { NzBackTopModule } from 'ng-zorro-antd/back-top';
+
+registerLocaleData(en);
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule,
+    /** NG_ZORRO */
+    NzMenuModule,
+    NzCollapseModule,
+    NzIconModule,
+    NzToolTipModule,
+    NzButtonModule,
+    NzLayoutModule,
+    NzBreadCrumbModule,
+    NzPageHeaderModule,
+    NzDividerModule,
+    NzSpaceModule,
+    NzAffixModule,
+    NzDropDownModule,
+    NzPaginationModule,
+    NzStepsModule,
+    NzAutocompleteModule,
+    NzCascaderModule,
+    NzCheckboxModule,
+    NzDatePickerModule,
+    NzFormModule,
+    NzInputModule,
+    NzInputNumberModule,
+    NzMentionModule,
+    NzRadioModule,
+    NzRateModule,
+    NzSelectModule,
+    NzSliderModule,
+    NzSwitchModule,
+    NzTimePickerModule,
+    NzTransferModule,
+    NzTreeSelectModule,
+    NzUploadModule,
+    NzAvatarModule,
+    NzBadgeModule,
+    NzCalendarModule,
+    NzCardModule,
+    NzCarouselModule,
+    NzCommentModule,
+    NzDescriptionsModule,
+    NzEmptyModule,
+    NzImageModule,
+    NzListModule,
+    NzPopoverModule,
+    NzStatisticModule,
+    NzTableModule,
+    NzTabsModule,
+    NzTagModule,
+    NzTimelineModule,
+    NzTreeModule,
+    NzTreeViewModule,
+    NzAlertModule,
+    NzDrawerModule,
+    NzMessageModule,
+    NzModalModule,
+    NzNotificationModule,
+    NzPopconfirmModule,
+    NzProgressModule,
+    NzResultModule,
+    NzSkeletonModule,
+    NzSpinModule,
+    NzAnchorModule,
+    NzBackTopModule
+  ],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
